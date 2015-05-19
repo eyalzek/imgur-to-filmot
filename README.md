506e40b202e80e7b6b74b3bab3c2bf6cabda5748
Stuart Davis <studavis@gmail.com>
2015-05-19 13:30:52 +0000

    Overall tidy up and addition of icons

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 
 As simple Chrome extension that changes all links on a webpage that contain imgur links to point to filmot.
 
-Useful if imgur is blocked at work / school.
+Useful if imgur is blocked at your work or school.
 
 Features:
 - Supports RES on Reddit (including never ending reddit)
-- Albums are changed to galleries so that they also work.
+- Rewrites https links to http as filmot doesn't support https (yet!)
+- Uses the filmot.com domain which has better ping times
+
+To do:
+- Add support for rewriting all links within filmot.com to a filmot.com path
+- Whitelist / blacklist sites
+- Add button to enable / disable on the fly
+- Optionally add other imgur mirror sites manually
--- swaplinks.js
+++ swaplinks.js
@@ -8,16 +8,20 @@ function changeLinks() {
     for (var i = 0; i < list.length; i++) {
         var node = list.item(i);
         node.setAttribute('href', node.getAttribute('href')
-            .replace('imgur.com/a/', 'filmot.org/gallery/'));
+            .replace('https://', 'http://'));
         node.setAttribute('href', node.getAttribute('href')
-            .replace('imgur.com', 'filmot.org'));
+            .replace('imgur.com/a/', 'filmot.com/a/'));
+        node.setAttribute('href', node.getAttribute('href')
+            .replace('imgur.com', 'filmot.com'));
     }
     list = document.querySelectorAll('[src*="imgur.com"]');
     for (i = 0; i < list.length; i++) {
         var node = list.item(i);
+        node.setAttribute('href', node.getAttribute('href')
+            .replace('https://', 'http://'));
         node.setAttribute('src', node.getAttribute('src')
-            .replace('imgur.com/a/', 'filmot.org/gallery/'));
+            .replace('imgur.com/a/', 'filmot.com/a/'));
         node.setAttribute('src', node.getAttribute('src')
-            .replace('imgur.com', 'filmot.org'));
+            .replace('imgur.com', 'filmot.com'));
     }
 }
