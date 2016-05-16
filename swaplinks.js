afe9e4b8728e6f3e87b29cd455b65acd20e10651
eyalzek <eyal.ez@gmail.com>
2016-05-16 20:33:13 +0200

    replace links inside the preview as well

--- swaplinks.js
+++ swaplinks.js
@@ -1,8 +1,8 @@
 changeLinks();
- 
+
 document.addEventListener('scroll', changeLinks);
 document.addEventListener('click', changeLinks);
- 
+
 function changeLinks() {
     var list = document.querySelectorAll('[href*="imgur.com"]');
     for (var i = 0; i < list.length; i++) {
@@ -23,5 +23,19 @@ function changeLinks() {
             .replace('imgur.com/a/', 'filmot.com/a/'));
         node.setAttribute('src', node.getAttribute('src')
             .replace('imgur.com', 'filmot.com'));
+    }
+    list = document.querySelectorAll('.expando[data-cachedhtml]');
+    for (i = 0; i < list.length; i++) {
+        var node = list.item(i);
+        var cachedhtml = node.dataset.cachedhtml;
+        if (cachedhtml.indexOf('imgur') !== -1) {
+            var p = new DOMParser;
+            var fixedUrl = p.parseFromString(cachedhtml, 'text/xml')
+                .getElementsByTagName('a')[0].getAttribute('href')
+                .replace('imgur.com', 'filmot.com');
+            node.dataset.cachedhtml = cachedhtml
+                .replace('imgur.com', 'filmot.com')
+                .replace(/(src\=")(?:(?=(\\?))\2.)*?(?=")/, '$1' + fixedUrl + '$2');
+        }
     }
 }
